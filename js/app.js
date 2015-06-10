5668e387e68af28ed00c085aba9717183143e2c2
Kevin Tan <thektan@gmail.com>
2015-06-09 23:54:29 -0700

    Integrate Parse

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 (function() {
+  /* Initialize Parse and Objects */
+  Parse.initialize("HkKD0qeE7y93LaBTO7lL2IIGbJVitPImljl5b3cV", "AMsA3zfBrUo78H3yFI63tuHWAZ5Ttr2KLbquFQQi");
+  var Event = Parse.Object.extend("Event");
+  var Activity = Parse.Object.extend("Activity");
+
   var app = angular.module('dayPlan', []);
 
   app.directive("newActivityModal", function() {
