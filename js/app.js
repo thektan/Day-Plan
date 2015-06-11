d0ee326e8e16d3096f65bceff8956113d9e479c2
Kevin Tan <thektan@gmail.com>
2015-06-11 16:13:17 -0700

    Add activity working with parent event object

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@
     /*$locationProvider.html5Mode(true);*/
   }]);
 
+  /* Navigation Bar Partial */
   app.directive("navigationBar", function() {
     return {
       restrict: "A",
@@ -30,19 +31,51 @@
     };
   });
 
+  /* New Activity Popup */
   app.directive("newActivityModal", function() {
     return {
       restrict: "A",
-      templateUrl: "partials/new-activity-modal.html"
+      templateUrl: "partials/new-activity-modal.html",
+      controller: function($scope) {
+        /*
+          Creates a new activity and persists it into the database.
+          @param activity the activity object to be added.
+          @param eventId the event object id the activity is linked to.
+        */
+        this.addActivity = function(activity, eventId) {
+          var activityObject = new Activity();
+          var event = new Event();
+          event.id = eventId;
+
+          activityObject.set("parent", event);
+          activityObject.set("name", activity.name);
+          activityObject.set("start", activity.start);
+          activityObject.set("end", activity.end);
+
+          activityObject.save(null, {
+            success: function(activityObject) {
+              $('#newActivityModal').modal('hide');
+              activityForm.reset();
+              alert('Successfully added new activity: ' + activityObject);
+            },
+            error: function(activityObject, error) {
+              alert('Failed to create new object, with error code: ' + error.message);
+            }
+          });
+
+        };
+      },
+      controllerAs: "activityFormCtrl"
     };
   });
 
+  /* New Event Form */
   app.directive("newEventForm", function() {
     return {
       restrict: "A",
       templateUrl: "partials/new-event-form.html",
       controller: function() {
-        /* Creates a new event and persists it into the database. */
+        // Creates a new event and persists it into the database.
         this.addEvent = function(event) {
           var eventObject = new Event();
 
@@ -64,10 +97,12 @@
     };
   });
 
+  /* Controller for the event page */
   app.controller('EventController', function($scope, $routeParams) {
-    $scope.eventObjectId = $routeParams.eventId;
+    $scope.eventObjectId = $routeParams.eventId; // Get the event id.
     $scope.eventObjectName = "";
 
+    // Retrieve the event using the id from the Parse database.
     var query = new Parse.Query(Event);
     query.get($scope.eventObjectId, {
       success: function(eventObject) {
